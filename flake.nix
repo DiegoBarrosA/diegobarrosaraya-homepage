dbb325eaae40d641810e0868c236d0b50de675ce
DiegoBarrosA <di.barros@pm.me>
2026-04-09 12:37:58 +0200

    Fix flake to support aarch64-linux

--- flake.nix
+++ flake.nix
@@ -8,21 +8,26 @@
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      mkDevShell =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_20
+            pkgs.pnpm
+          ];
+
+          shellHook = ''
+            echo "Diego's Homepage Dev Environment"
+            echo "Run: pnpm dev"
+            echo "Open: http://localhost:5173"
+          '';
+        };
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.nodejs_20
-          pkgs.pnpm
-        ];
-
-        shellHook = ''
-          echo "Diego's Homepage Dev Environment"
-          echo "Run: pnpm dev"
-          echo "Open: http://localhost:5173"
-        '';
-      };
+      devShells.x86_64-linux.default = mkDevShell "x86_64-linux";
+      devShells.aarch64-linux.default = mkDevShell "aarch64-linux";
     };
 }
